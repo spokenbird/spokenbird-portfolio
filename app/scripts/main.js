19ec2466a5eea6ea00738400bd21632e0a32a966
spokenbird <spokenbird@gmail.com>
2019-07-12 23:11:37 -0700

    Added Periods to messages Arr.
    Added periods to messages in typer.js message array.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,10 @@
 //messages to print
 let messages = [
-  'web developer',
-  'musician',
-  'dance facilitator',
-  'magician'
+  'web developer.',
+  'musician.',
+  'dance facilitator.',
+  'magician.'
 ];
 
 //print messages
-printLetters('changing-text', messages, undefined, undefined, undefined, 2000); //(target, messageArr, forward speed, backwards speed, new message delay, message remain time)
+printLetters('changing-text', messages, undefined, undefined, undefined, 2000); //(HTML Element target, messageArr, forward speed, backwards speed, new message delay, message remain time)
